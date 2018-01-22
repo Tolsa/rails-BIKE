c211ca3c3ce8e40e2e950c552b3ce75d4c861383
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-01-22 17:53:26 +0100

    getting the first page results

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,10 @@
+class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
 
   def home
+
+    @renderedText = 'scrape reddit data here'
+    require 'open-uri'
+    @doc = Nokogiri::HTML(open("https://www.leboncoin.fr/velos/offres/ile_de_france/paris/?th=1&q=v%E9lo%20course&location=Paris&pe=12"))
+
+    @entries = @doc.css('.list_item').first(8)
+    @entriestitle = []
+    @entrieslink = []
+    @entries.each do |entry|
+      title = entry['title']
+      link = entry['href']
+      Entry.create(title: title, link: link)
+    end
   end
 end
