28975348f70dfd881966eee4ced0a01f6a90007e
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-01-26 18:49:11 +0100

    fixing pics to bikes and layout start

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,37 +2,48 @@ class EntriesController < ApplicationController
   def index
     Entry.destroy_all
     require 'open-uri'
-    urldelta = 1
     # urlbasis = "https://www.leboncoin.fr/velos/offres/ile_de_france/paris/?o=#{urldelta}&q=v%E9lo%20course&pe=12&location=Paris"
     # @doc = Nokogiri::HTML(open(urlbasis))
     if (params[:query] != "")
+      urlbasis = "https://www.leboncoin.fr/velos/offres/ile_de_france/paris/?th=1&q=v%E9lo%20course%20taille%20#{params[:query]}&location=Paris&pe=12"
+      @doc = Nokogiri::HTML(open(urlbasis))
+      @entries = @doc.css('.list_item')
+      @entries.each do |entry|
+        title = entry['title']
+        link = "https:" + entry['href']
+        # @picture = @itemimage.attr("data-imgsrc")
+        # puts " ---------- #{@itemimage}"
+        @doc2 = Nokogiri::HTML(open("#{link}"))
+        @description = @doc2.css('.properties_description .value').inner_text
+        @test = params[:query]
 
-      while urldelta <= 6 do
-        urlbasis = "https://www.leboncoin.fr/velos/offres/ile_de_france/paris/?o=#{urldelta}&q=v%E9lo%20course&pe=12&location=Paris"
-        @doc = Nokogiri::HTML(open(urlbasis))
-        @entries = @doc.css('.list_item').first(12)
-        @entries.each do |entry|
-          title = entry['title']
-          link = entry['href']
-          @doc2 = Nokogiri::HTML(open("https:#{link}"))
-          @description = @doc2.css('.properties_description .value').inner_text
-          @itemimage = @doc2.css('span.lazyload')
-          @picture = @itemimage.attr("data-imgsrc")
-          puts " ---------- #{@picture}"
-          @test = params[:query]
-
-          if @description.include? @test
-            Entry.create(title: title, link: link, picture: @picture)
-          end
+        if @description.include? @test
+          Entry.create(title: title, link: link)
         end
-
-        urldelta += 1
-
       end
     end
 
 
+    allEntry = Entry.all
+    allEntry.each do |entry|
+      urlsecond = entry.link
+      @doc3 = Nokogiri::HTML(open(urlsecond))
+      @imglink = @doc3.css('.lazyload').to_s
+      re = /"https.*jpg"/
+      @imglinkregex = re.match(@imglink)
+      if @imglinkregex != nil
+        puts @imglinkregex[0]
+        @imgfine = @imglinkregex[0][1...-1]
+        entry.picture = @imgfine
+        entry.save
+      end
+      # if @imglinkok.include? @testbonus
+      #   entry.picture = @imglinkok
+      #   entry.save
+      # end
 
+
+    end
 
 
   end
